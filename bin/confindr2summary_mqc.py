8f37af69d0fe177b5465a2999f505d8b58d6e60f
Marc Hoeppner <mphoeppner@gmail.com>
2024-08-22 14:12:51 +0200

    Updating linting

--- bin/confindr2summary_mqc.py
+++ bin/confindr2summary_mqc.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import json
-import csv
 import glob
 import argparse
 
@@ -22,12 +21,12 @@ def main(output):
         "section_name": "ConfindR Summary",
         "description": "ConfindR identifies intra- and interspecific contaminations from read data",
         "plot_type": "table",
-        "pconfig": { "id": "confindr", "col1_header": "Sample"},
-        "data": { }
+        "pconfig": {"id": "confindr", "col1_header": "Sample"},
+        "data": {}
     }
 
     for report in reports:
-        with open(report,"r") as j:
+        with open(report, "r") as j:
             data = json.load(j)
             sample = data["sample"]
             confindr = data["confindr"]
@@ -35,10 +34,10 @@ def main(output):
             for read in confindr:
                 status = confindr[read]["contamStatus"]
                 if read in matrix["data"]:
-                      if status == "True":
-                          matrix["data"][sample]["Contaminated"] = "True"
+                    if status == "True":
+                        matrix["data"][sample]["Contaminated"] = "True"
                 else:
-                    matrix["data"][sample] = { "Contaminated": status }
+                    matrix["data"][sample] = {"Contaminated": status}
 
     with open(output, 'w') as fo:
         json.dump(matrix, fo)
